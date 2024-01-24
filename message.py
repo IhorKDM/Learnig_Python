f1f6ddd09cb56fe9a44551d34447401acd69e2a7
IhorKDM <132176962+IhorKDM@users.noreply.github.com>
2024-01-24 21:09:40 +0000

    Create message.py

---
+++ message.py
@@ -0,0 +1,15 @@
+# def hello():
+#     print('Hello! Have a good day')
+#
+# def bye():
+#     print('Bye, Have a good one')
+
+# class Car:
+# 
+#     wheels = 4
+# 
+#     def __init__(self, make, model, year, color):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.color = color
